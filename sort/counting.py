d355c0b67f78d222d4a769064848b9cde84849ab
ParkSeungWoo <tmd24710@naver.com>
2021-03-21 13:16:29 +0900

    modify counting to be able sorting negative numbers too

--- sort/counting.py
+++ sort/counting.py
@@ -1,25 +1,36 @@
 # counting sort
 
 
-r = 100  # range of numbers(1 ~ 5)
+# to sort only positive numbers
 data = [1, 3, 4, 4, 3, 3, 3, 3, 2, 2,
         2, 2, 5, 3, 2, 4, 5, 3, 2, 1,
         3, 5, 2, 4, 3, 2, 5, 3, 4, 5,
-        100, 34, 61, 57, 27, 9, 10, 10, 9, 27]
+        100, 34, 61, 57, 27, 9, 10, 10, 9, 0]
 
+# test data for sorting data which includes negative numbers
+data2 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10,
+         0, 2, 4, 1, 3, 7, 5, 6, 9, 10, 8]
 
-def countingSort(data, r):
-    count = [0] * r  # same as numpy.zeros(r)
+def countingSort(data):
+    mx, mn = abs(max(data)), abs(min(data))
+    maxd = mx if mx >= mn else mn
+    count = [0] * (maxd * 2 + 1)  # same as numpy.zeros()
     temp = []
-    for i in data:
-        count[i - 1] += 1
+    for i in data:  # count
+        count[i + maxd] += 1
+        print(f"{f'count[{i + maxd}] = {count[i + maxd]}': <50}// maxd : {maxd}, i : {i}")
 
-    for i in range(r):
-        for j in range(count[i]):
-            temp.append(i + 1)
+    for i in range(maxd * 2 + 1):  # insert datas
+        for _ in range(count[i]):
+            temp.append(i - maxd)
 
-    return temp
+    return temp  # return sorted data
 
 
-data = countingSort(data, r)
-print(data)
+# data = countingSort(data)
+# print(data)
+
+data2 = countingSort(data2)
+print(data2)
+
+
